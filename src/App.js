051c271f1036e630064fc92fdd7f2ff97b0766ca
Alan Rodriguez <info@alanrodriguez.me>
2020-04-04 21:25:45 -0700

    updating with the latest

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+import './App.css'
+import Wrapper from './Wrapper'
+
+let apiUrl = 'http://musicbox/api/'
+// let apiUrl = 'http://localhost/api/'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  let [ currentInfo, setCurrentInfo ] = useState(null)
+
+  useEffect(() => {
+    getPlayerInfo()
+    setInterval(getPlayerInfo, 3000)
+  }, [])
+
+  function getPlayerInfo() {
+    window.$.ajax({
+      url: apiUrl + 'player.php'
+    }).success(data => {
+      if (data) {
+        // console.log(data)
+        let { artist, title, album, duration, state, file } = data
+        let newData = { artist, title, album, duration, state, file }
+
+        if (!currentInfo) {
+          setCurrentInfo(newData)
+        } else if (data.title !== currentInfo.title) {
+          console.log({data})
+          setCurrentInfo(newData)
+        } 
+
+      } else {
+        setCurrentInfo(null)
+      }
+    })
+  }
+
+  return ( currentInfo ?
+    <Container>
+      <Wrapper data={ currentInfo } />      
+    </Container>
+    :
+    <div style={{ backgroundColor: 'red', width: '100px', height: '100px' }} />
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: black;
+`
